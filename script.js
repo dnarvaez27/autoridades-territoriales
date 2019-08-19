9a0375fe8642ef68f39b9a83b2e1c4282659b61f
David Narvaez <dnarvaez27@outlook.com>
2019-08-19 12:31:08 -0500

    about me

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ let closest10;
 let pos;
 
 let state;
+let timeout = [undefined];
 
 function home() {
-  if (!state || state === 2) {
+  if (!state || state !== 1) {
     state = 1;
     if (directionsRenderer) {
       directionsRenderer.setMap(null);
@@ -61,7 +62,7 @@ function home() {
 
 
 function goToMap() {
-  if (!state || state === 1) {
+  if (!state || state !== 2) {
     state = 2;
     document.getElementById('see-route').classList.remove('hidden');
 
@@ -103,6 +104,188 @@ function goToMap() {
         });
       }
     }, 1000);
+  }
+}
+
+
+function aboutme() {
+  if (!state || state !== 3) {
+    state = 3;
+
+    const config = {
+      size: { width: 500, height: 300 },
+      numParticles: 18,
+      radius: { min: 60, max: 120 },
+      speed: 1,
+      fps: 60
+    };
+
+    const random = {
+      range: (min, max) => Math.floor(Math.random() * (max - min)) + min,
+      simetricalRange: (a) => ((Math.random() > 0.5) ? -1 : 1) * ((Math.random() * a) + 1)
+    }
+
+    class Particle {
+      constructor(x, y, vx, vy, radius, key = undefined) {
+        this.x = x || 0;
+        this.y = y || 0;
+        this.vx = vx || 0;
+        this.vy = vy || 0;
+        this.radius = radius;
+        this.key = key;
+      }
+
+      update = (vx, vy) => {
+        this.x += this.vx + (vx || 0);
+        this.y += this.vy + (vy || 0);
+      }
+    }
+
+    class Generator {
+      constructor() {
+        const particles = [];
+        for (let i = 0; i < config.numParticles; i++) {
+          const x = config.size.width / 2;
+          const y = config.size.height / 2;
+          const vx = random.simetricalRange(config.speed);
+          const vy = random.simetricalRange(config.speed);
+          const radius = random.range(config.radius.min, config.radius.max)
+          const key = i;
+          const particle = new Particle(x, y, vx, vy, radius, key);
+          particles.push(particle);
+        }
+
+        this.particles = particles;
+      }
+
+      update = () => {
+        this.particles.forEach(particle => {
+          if (particle.x - particle.radius < 0) { particle.vx = -particle.vx; }
+          if (particle.x + particle.radius > config.size.width) { particle.vx = -particle.vx; }
+          if (particle.y - particle.radius < 0) { particle.vy = -particle.vy; }
+          if (particle.y + particle.radius > config.size.height) { particle.vy = -particle.vy; }
+          particle.update();
+        })
+      };
+    }
+
+    const filterGooeyColorBlending = (svg, id) => {
+      const defs = svg
+        .append("defs");
+      const filter = defs
+        .append("filter")
+        .attr("id", id);
+
+      filter
+        .append("feGaussianBlur")
+        .attr("in", "SourceGraphic")
+        .attr("stdDeviation", "10")
+        .attr("color-interpolation-filters", "sRGB")
+        .attr("result", "blur");
+
+      filter.append("feColorMatrix")
+        .attr("in", "blur")
+        .attr("mode", "matrix")
+        .attr("values", "1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9")
+        .attr("result", "gooey");
+    }
+
+    const redraw = (g, particles) => {
+      const particle = g
+        .selectAll("circle.particle")
+        .data(particles, d => d.key);
+
+      const color = d3
+        .scaleLinear()
+        .domain([0, particles.length])
+        .interpolate(d3.interpolateHcl)
+        .range([d3.rgb("#007AFF"), d3.rgb('#e91e63')]);
+
+      // Setup
+      particle
+        .enter()
+        .append("circle")
+        .attr("class", "particle")
+        .attr("r", d => d.radius)
+        .attr('fill', (_, i) => color(i));
+      // .attr('fill', 'black');
+
+      // Update
+      particle
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y);
+
+      particle
+        .exit()
+        .remove();
+    }
+
+    document.getElementById('see-route').classList.add('hidden');
+    const data = document.getElementById('data');
+    data.classList.add('home');
+    data.innerHTML = '';
+
+    const div = document.createElement('div');
+    div.setAttribute('id', 'aboutme-container');
+
+    const span = document.createElement('span');
+    span.appendChild(document.createTextNode('David Narvaez Guerrero'));
+
+    const span1 = document.createElement('span');
+    span1.appendChild(document.createTextNode('Estudiante de Ingeniería de Sistemas y Computación'));
+
+    const span2 = document.createElement('span');
+    span2.appendChild(document.createTextNode('Universidad de Los Andes'));
+
+    const alink = document.createElement('a');
+    alink.appendChild(document.createTextNode('Página Web'));
+    alink.setAttribute('href', 'https://dnarvaez27.github.io');
+    alink.setAttribute('target', '_blank');
+
+    div.appendChild(span);
+    div.appendChild(span1);
+    div.appendChild(span2);
+    div.appendChild(alink);
+
+    data.appendChild(div);
+
+    (function () {
+
+      const gen = new Generator();
+
+      window.requestAnimFrame = (
+        window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        (callback => window.setTimeout(callback, 1000 / config.fps))
+      );
+
+      // clearTimeout(timeout[0]);
+      window.cancelAnimationFrame(timeout[0])
+
+      const _id = 'gooeyCodeFilter';
+
+      const svg = d3
+        .select('#aboutme-container')
+        .append('svg');
+
+      filterGooeyColorBlending(svg, _id);
+
+      const g = svg.append('g')
+        .style("filter", `url(#${_id})`);
+
+      const tick = () => {
+        gen.update();
+        redraw(g, gen.particles);
+        // timeout[0] = window.setTimeout(tick, 1000/config.fps);
+        timeout[0] = window.requestAnimFrame(tick);
+      };
+
+      tick();
+      return svg.node()
+    })();
   }
 }
 
